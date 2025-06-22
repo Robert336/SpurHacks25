0516cf6cb44d0dc601d8486b47b5200384531edd
Yash Khanchandani <164212895+yashk-44@users.noreply.github.com>
2025-06-22 10:20:48 -0400

    Delete backend/app/composer.py

--- backend/app/composer.py
+++
@@ -1,8 +0,0 @@
-from authentication import *
-from groupmanager import *
-
-login("abcd@gmail.com", "George_123")
-print(f"uid = {get_uid()}")
-
-start_group("a", "b", "ccccccccc")
-print("sample script completed")
